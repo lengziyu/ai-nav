6a1bfbc164ff1049a2c74aa500e33965e7f1666f
lengziyu <1058566903@qq.com>
2023-04-01 07:50:10 +0800

    初始代码

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store';
+import router from './router';
+import heyui from 'heyui';
+import 'heyui/themes/index.less';
+
+// 创建vue实例
+const app = createApp(App);
+
+app.use(heyui);
+app.use(store);
+app.use(router);
+
+// 挂载实例
+app.mount('#app');
---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <headbar />
+        <sidebar />
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import headbar from '@/components/Headbar.vue';
+    import sidebar from '@/components/Sidebar.vue';
+</script>
